a6295b296e3af0ff3fd4ae96d02b0d9ce3bf0300
Virginia Sacudato <virginiasacudato@gmail.com>
2021-01-11 05:59:17 -0300

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GraficoComponent } from './grafico/grafico.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { VotarComponent } from './votar/votar.component';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GraficoComponent,
+    InicioComponent,
+    VotarComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/votar/votar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { VotarComponent } from './votar.component';
+
+describe('VotarComponent', () => {
+  let component: VotarComponent;
+  let fixture: ComponentFixture<VotarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VotarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(VotarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
