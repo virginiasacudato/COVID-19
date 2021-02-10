6d90198572b38e524b15f4c1558325207e678b33
Virginia Sacudato <virginiasacudato@gmail.com>
2021-02-10 16:11:19 -0300

    Consumo de API REST y muestra en pantalla

--- src/app/components/grafico/grafico.component.ts
+++ src/app/components/grafico/grafico.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+
+import { CovidService } from '../../services/covid.service';
+import { Chart } from 'chart.js';
+
 @Component({
   selector: 'app-grafico',
   templateUrl: './grafico.component.html',
   styleUrls: ['./grafico.component.css']
 })
 export class GraficoComponent {
-  
+  chart = [];
+
+  constructor( private covid: CovidService){}
+
+  ngOnInit(){
+    this.covid.covidApi().subscribe(res => console.log(res));
+  }
 }
 
 
--- src/app/services/covid.service.ts
+++ src/app/services/covid.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CovidService {
 
-  url = 'https://api.covid19api.com/country/argentina/status/confirmed';
-  constructor(private http: HttpClient) { } // Se inserta la clase HttpClient
+  constructor(private _http: HttpClient) { }
 
-  getdata(): Observable<any> {
-    return this.http.get(this.url);
+  covidApi(){
+    const urlApi = 'https://coronavirus-tracker-api.herokuapp.com/v2/locations/6'; // URL a API
+    return this._http.get(urlApi).pipe(map((response: any) => response)); //Recorro datos con map y los muestro en consola
   }
-    
 }
