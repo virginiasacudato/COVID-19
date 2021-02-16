adc29f8d49ba7bb58444eb1e662f93bbe6ae085c
Virginia Sacudato <virginiasacudato@gmail.com>
2021-02-16 17:02:25 -0300

    Inicialización de votación con Pusher

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 
+
 import { CovidService } from './services/covid.service';
 
 import { AppComponent } from './app.component';
--- src/app/pages/votar/votar.component.ts
+++ src/app/pages/votar/votar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-votar',
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VotarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  event = 'vote';
+      vote = '';
+      voted = false;
+      vaccineData = [
+        {
+          name: 'Sputnick V',
+          creator: 'Centro Gamaleya',
+          effectiveness: 90,
+          shortName: 'sputnikv',
+          image: 'https://www.dw.com/image/56053382_401.jpg'
+        },
+        {
+          name: 'Pfizer',
+          creator: 'BioNTech, Fosun Pharma, Pfizer',
+          effectiveness: 95,
+          shortName: 'pfizer',
+          image: 'https://www.dw.com/image/56004999_401.jpg',
+        },
+        {
+          name: 'AstraZeneca/Oxford',
+          creator: 'Universidad de Oxford y AstraZeneca',
+          effectiveness: 70,
+          shortName: 'oxford',
+          image: 'https://images.pagina12.com.ar/styles/focal_3_2_960x640/public/2021-02/141398-astrazeneca-oms.jpg?itok=dsPkWTSw',
+        }
+      ];
+      voteCount = {
+        sputnikv: 0,
+        pfizer: 0,
+        oxford: 0,
+      };
 
-  ngOnInit(): void {
-  }
+      castVote(vaccine) {
+        this.http
+          .post(`http://localhost:4000/api/v1/vote`, { vaccine }) // Enviamos por POST el nombre de la vacuna
+          .subscribe((res: any) => {
+            this.vote = res.vaccine;
+            this.voted = true; // Establecemos la propiedad voted en true
+          });
+      }
+
+      getVoteClasses(vaccine) {
+        return {
+          elect: this.voted && this.vote === vaccine,
+          lost: this.voted && this.vote !== vaccine,
+        };
+      }
+
+
+  ngOnInit(): void {}
 
 }
